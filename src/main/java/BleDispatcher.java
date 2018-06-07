077a860273a75369a37223a9c7a4768a544e3a27
Josh Siva <jtsiva@gmail.com>
2018-06-07 09:11:31 -0400

    added method of maintaining multiple read callbacks

--- src/main/java/BleDispatcher.java
+++ src/main/java/BleDispatcher.java
@@ -5,9 +5,46 @@ import nd.edu.bluenet_stack.Reader;
 
 public class BleDispatcher extends DummyBLE {
 	
+	private Map<byte[], Reader> mReaderMap = new HashMap<byte[], Reader>();
+	private List<Reader> mUnmappedReaders = new ArrayList<Reader>();
+
 
 	@Override
 	public void setReadCB(Reader reader) {
-		this.mReadCB = reader;
+		mUnmappedReaders.add(new Reader() {
+			public int read(AdvertisementPayload advPayload) {
+				mReaderMap.put(advPayload.getDestID(), reader);
+				return reader.read(advPayload);
+			}
+
+			public int read(String src, byte[] message) {
+				throw new java.lang.UnsupportedOperationException("Not supported.");
+			}
+		});
+			
+	}
+
+	@Override
+	public int write(AdvertisementPayload advPayload) {
+
+		if (mReaderMap.contains(advPayload.getDestID())) {
+			
+			if (null != advPayload.getOneHopNeighbor()) {
+				Reader reader = mReaderMap.get(advPayload.getOneHopNeighbor());
+				return reader.read(advPayload);
+			}
+			else {
+				for (Map.Entry<byte [], Reader> entry : mReaderMap.entrySet())	{
+				    entry.getValue().read(advPayload);
+				}
+			}
+		}
+		else {
+			for (Reader reader : mUnmappedReaders) {
+				reader.read(advPayload);
+			}
+		}
+		
+
 	}
 }
